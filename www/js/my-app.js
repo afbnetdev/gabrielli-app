94cd0a62a850bc8b1ef5fea1ecce168f275fb886
Elia <Elia@DESKTOP-6M0UBEV>
2017-11-17 09:41:44 +0100

    TEST

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -2,6 +2,7 @@
 var myApp = new Framework7({
     template7Pages: true,
     material: true,
+    uniqueHistory : true,
     preroute: function (view, options) {
         if (!window.sessionStorage.jsessionid) {
             getLogout();
