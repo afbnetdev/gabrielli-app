9340040915a3316685e9ea9fd4e25f763f564d32
Elia <Elia@DESKTOP-6M0UBEV>
2017-12-07 10:13:33 +0100

    TEST

--- www/js/util.js
+++ www/js/util.js
@@ -3,7 +3,7 @@
 Initial setup
  ---------------------------------------*/
 
-var URL_ENDPOINT = 'http://portal.gabriellispa.it';
+var URL_ENDPOINT = 'https://portal.gabriellispa.it';
 //var URL_ENDPOINT = 'http://192.168.2.90:9080';
 
 //FILTER STRING
