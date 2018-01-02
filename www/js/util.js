97ac0b41ce9c444c880a6cf5924f3731fcfc53d1
Elia <Elia@DESKTOP-6M0UBEV>
2018-01-02 10:48:40 +0100

    TEST

--- www/js/util.js
+++ www/js/util.js
@@ -143,7 +143,7 @@ function buildDocumentTable(myList, columns, limit, lastIndexDoc) {
          var linkPDF = e.currentTarget.getAttribute("data-linkpdf");
          //myApp.alert('url: '+linkPDF);
          if(linkPDF){
-          var ref = cordova.InAppBrowser.open(linkPDF, '_system', 'location=yes');
+          var ref = cordova.InAppBrowser.open(linkPDF, '_self', 'location=yes');
           //var ref = window.open(linkPDF, '_system', 'location=yes'); 
          }else{
              myApp.alert("Impossibile reperire il Pdf")
