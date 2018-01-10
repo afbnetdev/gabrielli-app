f34fc63c64fcf109811a71a6c4fb61550f141787
Elia <Elia@DESKTOP-6M0UBEV>
2018-01-10 17:58:41 +0100

    TEST

--- www/js/util.js
+++ www/js/util.js
@@ -3,10 +3,10 @@
 Initial setup
  ---------------------------------------*/
 
-var URL_ENDPOINT = 'http://portal.gabriellispa.it';
+var URL_ENDPOINT = 'https://portal.gabriellispa.it';
 //var URL_ENDPOINT = 'http://192.168.2.90:9080';
 //var TEST_URL = 'http://192.168.81.215:9080';
-var TEST_URL = 'http://portal.gabriellispa.it';
+var TEST_URL = 'https://portal.gabriellispa.it';
 
 //Funzione per settare un obj nel sessionStorage
 
